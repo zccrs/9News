91e72376c4899ed5a36e29443fb6c8b16ef40eb9
QShen3 <qazxdrcssc2006@163.com>
2014-12-11 12:56:21 +0800

    在BB10中加入星辰兄的网络请求部分

--- src/blackberry/applicationui.cpp
+++ src/blackberry/applicationui.cpp
@@ -1,9 +1,9 @@
-#include "applicationui.h"
-
 #include <bb/cascades/Application>
 #include <bb/cascades/QmlDocument>
 #include <bb/cascades/AbstractPane>
-
+#include "applicationui.h"
+#include "utility.h"
+#include "myhttprequest.h"
 using namespace bb::cascades;
 
 ApplicationUI::ApplicationUI(bb::cascades::Application *app) :
@@ -12,8 +12,9 @@ ApplicationUI::ApplicationUI(bb::cascades::Application *app) :
     // By default the QmlDocument object is owned by the Application instance
     // and will have the lifespan of the application
     QmlDocument *qml = QmlDocument::create("asset:///main.qml");
-
     // Create root object for the UI
+    Utility *utility = Utility::createUtilityClass();
+    qml->setContextProperty("utility", utility);
     AbstractPane *root = qml->createRootObject<AbstractPane>();
 
     // Set created root object as the application scene
