cc711a0b3fc6553d8012862517b198b716a32fdb
AfterTheRainOfStars <AfterTheRainOfStars@github.com>
2014-12-13 21:46:53 +0800

    增加关于页，增加新闻列表下拉刷新，增加刷新全部新闻菜单项，增加下拉更改新闻排列方式

--- qml/symbian/NewsListPage.qml
+++ qml/symbian/NewsListPage.qml
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import QtQuick 1.0 // to target S60 5th Edition or Maemo 5
 import QtQuick 1.1
 import com.nokia.symbian 1.1
@@ -25,59 +24,3 @@ NewsList{
         }
     }
 }
-=======
-// import QtQuick 1.0 // to target S60 5th Edition or Maemo 5
-import QtQuick 1.1
-import com.nokia.symbian 1.1
-import "../utility/newsListPage"
-
-NewsList{
-    id: root
-
-    footer: componentFooter
-
-    Component{
-        id: componentFooter
-
-        Item{
-            width: parent.width
-            height: textLoadMoreNews.implicitHeight+20
-            Text{
-                id: textLoadMoreNews
-                text: qsTr("load more...")
-                anchors.centerIn: parent
-                visible: newsList.count>1
-                color: command.invertedTheme?"black":"#ccc"
-
-                MouseArea{
-                    anchors.fill: parent
-                    enabled: newsList.isBusy
-                    onClicked: {
-                        textLoadMoreNews.color = "#888"
-                        newsList.addMoreNews()//增加新闻
-                    }
-                }
-            }
-            //newsList对象在MainPage中，因为在compoentFooter中无法引用的root对象，所以这也是不得已而为之
-        }
-    }
-
-    BusyIndicator {
-        id: busyIndicator
-        running: visible
-        visible: root.isBusy
-        anchors.centerIn: parent
-        width: 50
-        height: 50
-    }
-
-    ScrollBar {
-        platformInverted: command.invertedTheme
-        flickableItem: root
-        anchors {
-            right: parent.right
-            top: parent.top
-        }
-    }
-}
->>>>>>> 5eadeb2e4c633312e53c5ed6b7be596665fabe33
--- src/utility/monitormouseevent.cpp
+++ src/utility/monitormouseevent.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "monitormouseevent.h"
 #include <QGraphicsSceneMouseEvent>
 #include <QDebug>
@@ -59,61 +58,3 @@ bool MonitorMouseEvent::eventFilter(QObject *target, QEvent *event)
     return false;
 }
 #endif
-=======
-#include "monitormouseevent.h"
-#include <QGraphicsSceneMouseEvent>
-#include <QDebug>
-#include <QApplication>
-
-#if(QT_VERSION>=0x050000)
-MonitorMouseEvent::MonitorMouseEvent(QQuickPaintedItem *parent) :
-    QQuickPaintedItem(parent)
-#else
-MonitorMouseEvent::MonitorMouseEvent(QDeclarativeItem *parent) :
-    QDeclarativeItem(parent)
-#endif
-{
-    m_target = NULL;
-    setAcceptedMouseButtons(Qt::LeftButton);
-}
-
-QObject *MonitorMouseEvent::target() const
-{
-    return m_target;
-}
-
-void MonitorMouseEvent::setTarget(QObject *arg)
-{
-    if(arg!=m_target){
-        m_target = arg;
-        if(arg)
-            arg->installEventFilter(this);
-        setParent(arg);
-        emit targetChanged(arg);
-    }
-}
-
-#if(QT_VERSION>=0x050000)
-#else
-void MonitorMouseEvent::mousePressEvent(QGraphicsSceneMouseEvent *event)
-{
-    emit mousePress(event->pos());
-    QDeclarativeItem::mousePressEvent(event);
-}
-
-bool MonitorMouseEvent::eventFilter(QObject *target, QEvent *event)
-{
-    if(target==m_target){
-        QGraphicsSceneMouseEvent* mouse_event = static_cast<QGraphicsSceneMouseEvent*>(event);
-
-        if(event->type()==QEvent::GraphicsSceneMouseRelease||event->type()==QEvent::MouseButtonRelease){
-            emit mouseRelease(mouse_event->pos());
-        }else if(event->type()==QEvent::GraphicsSceneMouseDoubleClick||event->type()==QEvent::MouseButtonDblClick){
-            emit mouseDoubleClicked(mouse_event->pos());
-        }
-    }
-
-    return false;
-}
-#endif
->>>>>>> 5eadeb2e4c633312e53c5ed6b7be596665fabe33
